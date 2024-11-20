cb15b05f2b7c0271aeca81c9cfc1f98906e80c85
hilosync <62771262+hilosync@users.noreply.github.com>
2024-11-20 17:50:12 +0000

    Time: 7 ms (90.27%) | Memory: 18.2 MB (92.34%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -2,35 +2,26 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         if len(height) < 3:
             return 0
-        
-        l, r = 0, 1
-        rightMax = height[r]
+
+        l, r = 0, len(height) - 1
+        maxLeft = height[l]
+        maxRight = height[r]
 
         totalWater = 0
-        def findMax(rightPointer):
-            index = rightPointer
-            currMax = 0
-            for i in range(rightPointer+1, len(height)):
-                if height[i] >= currMax:
-                    currMax = height[i]
-                    index = i
+
+        while l < r:
+            if maxLeft <= maxRight:
+                totalWater += maxLeft-height[l]
+                l += 1
+                maxLeft = max(maxLeft, height[l])
             
-            return index
-
-
-
-        for i in range(1,len(height)-1):
-            
-            if i == r:
-                r = findMax(r)
-            
-            currentWater = min(height[l], height[r]) - height[i]
-
-            if currentWater > 0:
-                totalWater += currentWater
-        
-            if height[i] > height[l]:
-                l = i
+            else:
+                totalWater += maxRight-height[r]
+                r -= 1
+                maxRight = max(maxRight, height[r])
 
         return totalWater
+
+
+
             
