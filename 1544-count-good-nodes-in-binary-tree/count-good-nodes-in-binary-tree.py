ef874b8c04d9b773edeb4f58fb264933b2150a28
hilosync <62771262+hilosync@users.noreply.github.com>
2024-11-21 12:50:32 +0000

    Time: 127 ms (70.23%) | Memory: 30.8 MB (91.60%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -9,36 +9,23 @@ class Solution:
         if not root:
             return None
         
-        
         result = 0
-        i = 0
         current = root
-        nodeStack = [i]
-        currMax = current.val
 
-        parentMax = {i:[current,currMax]}
-
+        nodeStack = [[current, current.val]]
 
         while nodeStack:
-            temp = nodeStack.pop()
-            current = parentMax[temp][0]
-            currMax = parentMax[temp][1]
+            current, currMax = nodeStack.pop()
 
             if current.val >= currMax:
                 result += 1
                 currMax = current.val
-            
+
             if current.left:
-                i += 1
-                nodeStack.append(i)
-                parentMax[i] = [current.left, currMax]
-
+                nodeStack.append([current.left, currMax])
             if current.right:
-                i += 1
-                nodeStack.append(i)
-                parentMax[i] = [current.right, currMax]
-
-
+                nodeStack.append([current.right, currMax])
+        
         return result
 
 
