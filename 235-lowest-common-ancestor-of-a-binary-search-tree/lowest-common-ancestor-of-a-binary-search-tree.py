ec170ca64a13acde88d4d8e706d339e0aff6cedf
hilosync <62771262+hilosync@users.noreply.github.com>
2024-11-28 22:06:02 +0000

    Time: 58 ms (36.73%) | Memory: 20.9 MB (19.07%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,31 +7,18 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if not root:
-            return None
-        def dfs(node, parents, target):
-            parents = list(parents)
-            if not node:
-                return None
-            parents.append(node)
-            if node == target:
-                return parents
+        currNode = root
 
-            return dfs(node.left, parents, target) or dfs(node.right, parents, target)
-
-        parentNodesP = dfs(root, [], p)
-        parentNodesQ = dfs(root, [], q)
-
-        i, j = 0, 0
-
-        while i < len(parentNodesP) and j < len(parentNodesQ):
-            if parentNodesP[i] == parentNodesQ[j]:
-                result = parentNodesP[i]
+        while currNode:
+            if currNode.val < min(p.val,q.val):
+                currNode = currNode.right
+            elif currNode.val > max(p.val, q.val):
+                currNode = currNode.left
             else:
-                break
-            i += 1
-            j += 1
-        return result
+                return currNode
+        
+        return None
+        
 
         
 
