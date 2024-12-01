2593e3940d3061f503b7ec6aca7b81ce414f1ad8
hilosync <62771262+hilosync@users.noreply.github.com>
2024-12-01 15:20:32 +0000

    Time: 1 ms (78.44%) | Memory: 18.1 MB (12.46%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -6,12 +6,24 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        def dfs(root):
-            if not root:
-                return [True, 0]
+        if not root:
+            return True
+        
+        nodeStack = [root]
+        nodeDict = {None: 0}
+    
+        while nodeStack:
+            curr = nodeStack[-1]
+            if curr.left and curr.left not in nodeDict:
+                nodeStack.append(curr.left)
+            elif curr.right and curr.right not in nodeDict:
+                nodeStack.append(curr.right)
+            else:
+                curr = nodeStack.pop()
+                left, right = nodeDict[curr.left], nodeDict[curr.right]
+                if abs(left-right) > 1:
+                    return False
 
-            left, right = dfs(root.left), dfs(root.right)
-            balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
-            return [balanced, 1 + max(left[1], right[1])]
+                nodeDict[curr] = max(left,right) + 1
 
-        return dfs(root)[0]
+        return True
